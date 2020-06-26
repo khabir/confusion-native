420d3a145b9341699e4cb96f848691f93140af36
Md Khabir Uddin <imostad@gmail.com>
2020-06-26 20:54:37 +0600

    Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -178,3 +178,8 @@ export const addComment = (comment) => {
         payload: comment
     })
 };
+
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+});  
